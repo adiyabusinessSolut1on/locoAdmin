02c68b668accdcb901089166eb1e1a521175f947
Mohammad Shahzad <shahzad201415@gmail.com>
2024-07-16 17:46:53 +0530

    changes

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -13,6 +13,7 @@ interface LoginData {
   email: string;
   password: string;
   role: string;
+  otp?:number
 }
 const Login = () => {
   const navigate = useNavigate();
@@ -21,7 +22,10 @@ const Login = () => {
     email: "",
     password: "",
     role: "admin",
+    otp:undefined
   });
+
+  const [isOtps,setIsotp]=useState(false)
   const [isVisible, setVisible] = useState(false);
   const dispatch = useDispatch();
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,19 +35,28 @@ const Login = () => {
       [name]: value,
     }));
   };
-
-  const handleSubmit = (e: React.FormEvent) => {
+const backLogin=()=>{
+  setLoginData({
+    email: "",
+    password: "",
+    role: "admin",
+    otp:undefined
+  })
+  setIsotp(false)
+}
+  const verifyOtp = (e: React.FormEvent) => {
     e.preventDefault();
     createPost({
-      data: {...logindata},
-      path: "/login",
+      data: {email:logindata?.email,otp:Number(logindata?.otp)},
+      path: "/loginotp/verify",
     })
       .then((res) => {
         console.log(res);
-        if (res?.data?.success) {
+        if (res?.data?.success&&res?.data?.token) {
+         
           localStorage.setItem("user", res?.data?.token);
           dispatch(setUserToken(res?.data?.token));
-          toast.success("You have been Logged in", {
+          toast.success(res?.data?.message, {
             autoClose: 3000,
           });
           setTimeout(() => {
@@ -57,11 +70,96 @@ const Login = () => {
         toast.error(`Data is wrong`);
       });
   };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    createPost({
+      data: {...logindata},
+      path: "/login",
+    })
+      .then((res) => {
+        console.log(res);
+        if (res?.data?.success) {
+          setIsotp(res?.data?.success)
+          // localStorage.setItem("user", res?.data?.token);
+          // dispatch(setUserToken(res?.data?.token));
+          toast.success(res?.data?.message, {
+            autoClose: 3000,
+          });
+          // setTimeout(() => {
+          //   navigate("/");
+          // }, 2000);
+        } else {
+          toast.error("Some eror occure check your cridential");
+        }
+      })
+      .catch(() => {
+        toast.error(`Data is wrong`);
+      });
+  };
 
   return (
     <React.Fragment>
       <ToastContainer/>
-      <div className="flex flex-col items-center justify-center h-screen px-4 mx-auto  w-[100%]  sm:w-[80%] md:w-[50%]  lg:w-[40%]">
+      
+     {isOtps?<div className="flex flex-col items-center justify-center h-screen px-4 mx-auto  w-[100%]  sm:w-[80%] md:w-[50%]  lg:w-[40%]">
+        <div className="">
+          <h4 className="text-[24px] md:text-3xl lg:text-4xl font-bold pb-2 text-gray-600">
+            Enter OTP
+          </h4>
+         
+        </div>
+        <form
+          className="flex flex-col items-center justify-center w-full py-4 lg:p-8"
+          onSubmit={verifyOtp}
+        >
+         
+          <div className="relative w-full mb-4 lg:mb-6">
+            <FiLock className="absolute w-5 h-5 text-gray-400 top-[10px] left-2" />
+            <input
+              className="w-full px-8 py-[6px]  bg-blue-50 transition-all duration-200 border-2 rounded-md outline-none placeholder:text-gray-400 pl-9 focus:border-blue-300"
+              onChange={handleChange}
+              value={logindata?.otp}
+              name="otp"
+              type="number" 
+              required
+              placeholder="OTP"
+            />
+            <button
+              className="absolute text-gray-400 right-4 top-[10px]"
+              role="button"
+              onClick={(e) => {
+                e.preventDefault();
+                setVisible((prev) => !prev);
+              }}
+            >
+              {isVisible ? (
+                <FiEye className="w-5 h-5 text-blue-400" />
+              ) : (
+                <FiEyeOff className="w-5 h-5 " />
+              )}
+            </button>
+          </div>
+
+          <div className="grid w-full pb-2">
+            
+            <button
+              type="submit"
+              className="px-4 py-2 text-white bg-[#1F3C88] border rounded-md disabled:bg-gray-600 hover:bg-[#1f5e88]"
+            >
+              Submit
+            </button>
+          </div>
+          <div className="mt-2 text-sm ">
+            
+            <div
+              onClick={backLogin}
+              className="cursor-pointer ml-2 text-blue-500 font-medium  border-b border-transparent   hover:text-blue-600 transition-all duration-500  w-[fit-content]"
+            >
+              Back to Login
+            </div>
+          </div>
+        </form>
+      </div>:<div className="flex flex-col items-center justify-center h-screen px-4 mx-auto  w-[100%]  sm:w-[80%] md:w-[50%]  lg:w-[40%]">
         <div className="">
           <h4 className="text-[24px] md:text-3xl lg:text-4xl font-bold pb-2 text-gray-600">
             Log in to your Account
@@ -139,7 +237,8 @@ const Login = () => {
             </Link>
           </div>
         </form>
-      </div>
+      </div>} 
+     
       <div className="flex-col items-center justify-center hidden h-screen rounded-md md:flex">
         <img src={loginImg} alt="login image" className="w-full h-full" />
       </div>
